3cf3c4f9e48a8f723b79c7d417a5540cb755a5ff
Alex <Alex@DESKTOP-H4PC7U1.fritz.box>
2017-02-06 16:15:45 +0100

    clean code und anwendung auf server bereitgestellt

--- webanwendung/main.js
+++ webanwendung/main.js
@@ -6,11 +6,11 @@ var map;
 var sliderData;
 var bosMarkerList = new L.FeatureGroup();
 var boolGeojsonErreichbarkeitspolygon = false, boolBosMarkerList = false;
-
+var geojsonErreichbarkeitspolygon;
 
  jQuery(document).ready(function(){
-	jQuery("#btn_polygon").prop('disabled', true);
-	jQuery("#btn_delete").prop('disabled', true);
+	jQuery("#btnPolygon").prop('disabled', true);
+	jQuery("#btnDelete").prop('disabled', true);
     jQuery(".slider").slider({
         max: 25,
         min: 5,
@@ -24,56 +24,54 @@ var boolGeojsonErreichbarkeitspolygon = false, boolBosMarkerList = false;
         rest: "label",
         suffix: "Min.",
     });
-    sliderData = jQuery(".slider").slider("value"); //der Startwert wird der Variablen slider_number_last_dates hinzugefügt
-    getMap();
+    sliderData = jQuery(".slider").slider("value"); //der Startwert wird der Variablen sliderData hinzugefügt
+    getMap();  
     
-    
-    
-    jQuery("#btn_information").click(function(){
-        document.getElementById('modal_header_information').innerHTML = "<h4 class='modal-title'>Information zur Webanwendung</h4>";
-        document.getElementById('modal_body_information').innerHTML =  "<div class='info_content'>Software Engineering Projekt der Hochschule Mainz - Semester 1 Master - WS 2016/2017<br><br>Verantwortlich für den Inhalt der Webseite:<br><br>Angelique Prüß, Sandro Mertens, Thomas Müller, Alexander Rolwes<br>Anfragen bitte an info@bos-erreichbarkeitsanalyse.de</div>";
-        jQuery("#modal_information").modal();
+    jQuery("#btnInformation").click(function(){
+        document.getElementById('modalHeaderInformation').innerHTML = "<h4 class='modal-title'>Information zur Webanwendung</h4>";
+        document.getElementById('modalBodyInformation').innerHTML =  "<div>Software Engineering Projekt der Hochschule Mainz - Semester 1 Master - WS 2016/2017<br><br>Verantwortlich für den Inhalt der Webseite:<br><br>Angelique Prüß, Sandro Mertens, Thomas Müller, Alexander Rolwes<br>Anfragen bitte an info@bos-erreichbarkeitsanalyse.de</div>";
+        jQuery("#modalInformation").modal();
+    });   
+	
+    jQuery("#btnSearch").click(function(){ 
+        var searchData = jQuery("#formAdress").val();
+		if (searchData.length > 0){
+			var urlGeocoder = "http://143.93.114.139/geocoder";
+			jQuery.ajax({
+				type: 'GET',
+				dataType: 'jsonp',
+				url: urlGeocoder,
+				crossDomain : true,
+				data: 'queryString='+searchData+'&locale=de',
+				xhrFields: {withCredentials: true},
+				success: function(dataGeocoder){
+					if (dataGeocoder.features.length > 0 && searchData !=""){
+						longMapcenter = dataGeocoder.features[0].geometry.coordinates[0];
+						latMapcenter = dataGeocoder.features[0].geometry.coordinates[1];
+						map.setView(new L.LatLng(latMapcenter, longMapcenter), 10);
+					}else{
+						document.getElementById('modalHeaderAlert').innerHTML = "<h4 class='modal-title'>Achtung!</h4>";
+						document.getElementById('modalBodyAlert').innerHTML =  "<div>Die von Ihnen eingegeben Adresse ist fehlerhaft. Es konnte kein passender Ort zugeordnet werden. Bitte verändern Sie Ihren Eingabe und führen Sie eine erneute Suchanfrage durch.</div>";
+						jQuery("#modalAlert").modal();
+					}    
+				}
+			})
+		}else{
+			document.getElementById('modalHeaderAlert').innerHTML = "<h4 class='modal-title'>Achtung!</h4>";
+			document.getElementById('modalBodyAlert').innerHTML =  "<div>Das Suchformular ist leer. Bitte geben Sie eine Adresse ein.</div>";
+			jQuery("#modalAlert").modal();
+		}
     });
     
-    jQuery("#btn_search").click(function(){
-        
-        var searchData = jQuery("#form_adress").val();
-        var urlGeocoder = "http://143.93.114.139/geocoder";
-       
-        jQuery.ajax({
-            type: 'GET',
-            dataType: 'jsonp',
-            url: urlGeocoder,
-            crossDomain : true,
-            data: 'queryString='+searchData+'&locale=de',
-            xhrFields: {withCredentials: true},
-            success: function(dataGeocoder){
-				if (dataGeocoder.features.length > 0){
-					longMapcenter = dataGeocoder.features[0].geometry.coordinates[0];
-					latMapcenter = dataGeocoder.features[0].geometry.coordinates[1];
-                
-					map.setView(new L.LatLng(latMapcenter, longMapcenter), 10);
-				}else{
-					document.getElementById('modal_header_alert').innerHTML = "<h4 class='modal-title'>Achtung!</h4>";
-					document.getElementById('modal_body_alert').innerHTML =  "<div class='info_content'>Die von Ihnen eingegeben Adresse ist fehlerhaft. Es konnte kein passender Ort zugeordnet werden. Bitte verändern Sie Ihren Eingabe und führen Sie eine erneute Suchanfrage durch.</div>";
-					jQuery("#modal_alert").modal();
-				}    
-            }
-        })
-    });
-    
-    jQuery("#btn_bos").click(function(){
-		
+    jQuery("#btnBos").click(function(){
         var urlBosStandorte = "http://143.93.114.120/overpassAPI";     //Adresse des MicroServices
         var queryIntrest = "fire_station";
-        
 		if (boolGeojsonErreichbarkeitspolygon == true){
 			geojsonErreichbarkeitspolygon.clearLayers();
 		};
 		if (boolBosMarkerList == true){
 			bosMarkerList.clearLayers();
 		};
-		
         var eastKoord = map.getBounds().getEast();
         var westKoord = map.getBounds().getWest();
         var southKoord = map.getBounds().getSouth();
@@ -83,7 +81,7 @@ var boolGeojsonErreichbarkeitspolygon = false, boolBosMarkerList = false;
 			timeout: 15000,
             type: 'GET',           		 //Übergabetyp: Get
             dataType: 'jsonp',           //Übergabe erfolgt im jsonp-Format
-            url: urlBosStandorte,      //Adresse des MicroServices (oben)
+            url: urlBosStandorte,        //Adresse des MicroServices (oben)
             crossDomain: true,           //damit er auch auf andere Server zugreifen kann
             data: 'interest='+queryIntrest+'&south='+southKoord+'&west='+westKoord+'&north='+northKoord+'&east='+eastKoord+'',
             xhrFields: {withCredentials: true},
@@ -106,30 +104,28 @@ var boolGeojsonErreichbarkeitspolygon = false, boolBosMarkerList = false;
 					}
 					map.addLayer(bosMarkerList); 
 					boolBosMarkerList = true;
-					jQuery("#btn_polygon").prop('disabled', false);
-					jQuery("#btn_delete").prop('disabled', false);
+					jQuery("#btnPolygon").prop('disabled', false);
+					jQuery("#btnDelete").prop('disabled', false);
 				}else{
-					document.getElementById('modal_header_alert').innerHTML = "<h4 class='modal-title'>Achtung!</h4>";
-					document.getElementById('modal_body_alert').innerHTML =  "<div class='info_content'>In dem von Ihnen ausgewählten Bereich stehen keine BOS-Standorte zur Verfügung. Bitte verändern Sie Ihren Kartenausschnitt und führen Sie eine erneute Suchanfrage durch.</div>";
-					jQuery("#modal_alert").modal();
+					document.getElementById('modalHeaderAlert').innerHTML = "<h4 class='modal-title'>Achtung!</h4>";
+					document.getElementById('modalBodyAlert').innerHTML =  "<div>In dem von Ihnen ausgewählten Bereich stehen keine BOS-Standorte zur Verfügung. Bitte verändern Sie Ihren Kartenausschnitt und führen Sie eine erneute Suchanfrage durch.</div>";
+					jQuery("#modalAlert").modal();
 				}	 	 
             },
 			error: function(){
-				document.getElementById('modal_header_alert').innerHTML = "<h4 class='modal-title'>Achtung! Die maximale Wartezeit wurde überschritten!</h4>";
-				document.getElementById('modal_body_alert').innerHTML =  "<div class='info_content'>In dem von Ihnen ausgewählten Bereich stehen eine Vielzahl an BOS-Standorte zur Verfügung. Bitte verkleinern Sie Ihren Kartenausschnitt und führen Sie eine erneute Suchanfrage durch.</div>";
-				jQuery("#modal_alert").modal();
+				document.getElementById('modalHeaderAlert').innerHTML = "<h4 class='modal-title'>Achtung! Die maximale Wartezeit wurde überschritten!</h4>";
+				document.getElementById('modalBodyAlert').innerHTML =  "<div>In dem von Ihnen ausgewählten Bereich stehen eine Vielzahl an BOS-Standorte zur Verfügung. Bitte verkleinern Sie Ihren Kartenausschnitt und führen Sie eine erneute Suchanfrage durch.</div>";
+				jQuery("#modalAlert").modal();
 			}
        })
     });
 
-    jQuery("#btn_polygon").click(function(){
-        var queryPoly = createObjPoly();
-    
-        var urlIsochrone = "http://143.93.114.120/isochrone";
-        
+    jQuery("#btnPolygon").click(function(){
+        var queryPoly = createObjPoly();   
+        var urlIsochrone = "http://143.93.114.120/isochrone";        
 		if (boolGeojsonErreichbarkeitspolygon == true){
 			geojsonErreichbarkeitspolygon.clearLayers();
-		};
+		}
         jQuery.ajax({
 			timeout: 15000,
 			type: 'POST',
@@ -149,22 +145,22 @@ var boolGeojsonErreichbarkeitspolygon = false, boolBosMarkerList = false;
 				// }else if (dataPoly == "error"){
 					// alert("error");
 				// }else{ 
-					var geojsonErreichbarkeitspolygon = L.geoJson(dataPoly).addTo(map);
+					geojsonErreichbarkeitspolygon = L.geoJson(dataPoly).addTo(map);
 					boolGeojsonErreichbarkeitspolygon = true;
 				// }
 			},
 			error: function(){
-				document.getElementById('modal_header_alert').innerHTML = "<h4 class='modal-title'>Achtung! Die maximale Wartezeit wurde überschritten!</h4>";
-				document.getElementById('modal_body_alert').innerHTML =  "<div class='info_content'>In dem von Ihnen ausgewählten Bereich stehen eine Vielzahl an BOS-Standorte zur Verfügung. Eine Abfrage des Erreichbarkeitspolygons kann nicht ausgeführt werden. <br>Bitte verkleinern Sie Ihren Kartenausschnitt oder verändern Sie ihre Zeitliche Hilfsfrist und führen Sie eine erneute Suchanfrage durch.</div>";
-				jQuery("#modal_alert").modal();
+				document.getElementById('modalHeaderAlert').innerHTML = "<h4 class='modal-title'>Achtung! Die maximale Wartezeit wurde überschritten!</h4>";
+				document.getElementById('modalBodyAlert').innerHTML =  "<div>In dem von Ihnen ausgewählten Bereich stehen eine Vielzahl an BOS-Standorte zur Verfügung. Eine Abfrage des Erreichbarkeitspolygons kann nicht ausgeführt werden. <br>Bitte verkleinern Sie Ihren Kartenausschnitt oder verändern Sie ihre Zeitliche Hilfsfrist und führen Sie eine erneute Suchanfrage durch.</div>";
+				jQuery("#modalAlert").modal();
 			}
         })   
-    });
-    
-    jQuery("#btn_delete").click(function(){
+    });  
+	
+    jQuery("#btnDelete").click(function(){
         bosMarkerList.clearLayers();
-		jQuery("#btn_polygon").prop('disabled', true);
-		jQuery("#btn_delete").prop('disabled', true);
+		jQuery("#btnPolygon").prop('disabled', true);
+		jQuery("#btnDelete").prop('disabled', true);
 		if (boolGeojsonErreichbarkeitspolygon == true){
 			geojsonErreichbarkeitspolygon.clearLayers();
 		}
@@ -185,7 +181,7 @@ function getMap(){
 function createObjPoly(){
     var obj = new Object();
     obj.timelimit = sliderData;
-    bos = [];
+    var bos = [];
     var i = 0;
     for (var fireMarker in bosMarkerList._layers){
         if (fireMarker._latlng !== null) {
